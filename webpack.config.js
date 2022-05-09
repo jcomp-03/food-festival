a4b2ce5f634540642fa11326a1a63b82be2143b8
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-09 00:19:30 -0400

    Removed 'min' suffix from img/ files; installed webpack for bundling up dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require("webpack");
+//For a basic configuration, we need to provide webpack
+// with three properties: entry, output, and mode.
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+          }),
+    ],
+    mode: 'development'
+
+};
